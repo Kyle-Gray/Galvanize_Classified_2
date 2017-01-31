54ed8490f0b2f9f636600c3eede7e012b3dcb15c
Kyle Gray <th33nd3r@gmail.com>
2017-01-30 17:08:13 -0700

    changed production route

--- knexfile.js
+++ knexfile.js
@@ -11,7 +11,8 @@ module.exports = {
     connection: 'postgres://localhost/classifieds_test'
    },
   production: {
-    connection:process.env.DATABASE_URL
+    connection:'postgres://xnyylznuisnnzs:62900348398f198d91b99e70b364ba1e6669073e6c74ee1248d16bfc9a2d2169@ec2-50-17-207-16.compute-1.amazonaws.com:5432/d7m6a0aeeunfrr'
+
   },
 
 };
